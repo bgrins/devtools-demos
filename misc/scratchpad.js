9b691c527aa5c96fd0a4cd0d40750a89310cce32
Brian Grinstead <briangrinstead@gmail.com>
2022-07-19 09:47:12 -0700

    update symbol species test

--- misc/scratchpad.js
+++ misc/scratchpad.js
@@ -2,21 +2,33 @@ let sym = Symbol;
 let spec = sym.species;
 
 class A extends Array {
-  static [spec] = Array;
+  static get [spec]() {
+    return Array;
+  }
 }
 
 let typeA = A.from([1, 2, 3]);
 
 let typeAArray = A.from([1, 2, 3]) // return type: A
   .map((x) => x + 1); // return type: Array
-console.log(typeA, typeAArray);
+console.log(typeA instanceof A, typeA, typeAArray instanceof A, typeAArray);
 
 class B extends Array {
-  static [Symbol.species] = Array;
+  static get [Symbol.species]() {
+    return Array;
+  }
 }
 
-let typeB = A.from([1, 2, 3]);
+let typeB = B.from([1, 2, 3]);
 
-let typeBArray = A.from([1, 2, 3]) // return type: A
+let typeBArray = B.from([1, 2, 3]) // return type: A
   .map((x) => x + 1); // return type: Array
-console.log(typeB, typeBArray);
+console.log(typeB instanceof B, typeB, typeBArray instanceof B, typeBArray);
+
+class C extends Array {}
+
+let typeC = C.from([1, 2, 3]);
+
+let typeCArray = C.from([1, 2, 3]) // return type: C
+  .map((x) => x + 1); // return type: C
+console.log(typeC instanceof C, typeC, typeCArray instanceof C, typeCArray);
